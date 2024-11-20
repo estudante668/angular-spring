243118359afc31e431b51e53c1d9bba69b10a60a
fabricionetto <agostinhonetto73@gmail.com>
2024-11-20 10:44:09 -0300

    Material table

--- src/app/cursos/curso/curso.component.ts
+++ src/app/cursos/curso/curso.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Curso } from '../../curso/objetos/curso';
+import {MatTableModule} from '@angular/material/table'; 
 
 @Component({
   selector: 'app-curso',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './curso.component.html',
   styleUrl: './curso.component.scss'
 })
 export class CursoComponent {
+  
+  displayedColumns: string[] = ['nome', 'curso'];
+  cursos: Curso[] = [
+    {id:'1', nome:'Bruno', categoria:'Eng. Mecanica'}
+  ];
 
 }
