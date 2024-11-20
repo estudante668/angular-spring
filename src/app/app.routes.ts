db5fd93edd1016fef598d378d118a5bb1af60dac
fabricionetto <agostinhonetto73@gmail.com>
2024-11-20 10:09:21 -0300

    Angular roteador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CursoComponent } from './cursos/curso/curso.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: CursoComponent,
+        title: 'Curso Componente'
+    }
+];
